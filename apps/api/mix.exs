e26ee71972a8767a8128836b2cd3824871aa1ee5
Randy Secrist <rsecrist@sofi.org>
2019-07-11 21:33:55 -0600

    add memory constraints, declare elixir 1.8 only

--- apps/api/mix.exs
+++ apps/api/mix.exs
@@ -4,7 +4,7 @@ defmodule API.Mixfile do
   def project do
     [app: :api,
      version: "0.0.1-dev",
-     elixir: "~> 1.6",
+     elixir: "~> 1.8",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
